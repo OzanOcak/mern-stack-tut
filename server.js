a1e2a072092ef6fd3ef58f4268ceda0684da4e0c
OzanOcak <ozanocak@gmail.com>
2022-09-01 15:17:38 -0400

    routes

--- server.js
+++ server.js
@@ -1,21 +1,33 @@
-const cookieParser = require("cookie-parser");
+require("dotenv").config();
 const express = require("express");
 const app = express();
 const path = require("path");
-const corsOptions = require("./config/corsOption");
-const errorHandler = require("./middleware/errorHandling");
+const { logger, logEvents } = require("./middleware/logger");
+const errorHandler = require("./middleware/errorHandler");
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
-const { logger } = require("./middleware/logger");
+const corsOptions = require("./config/corsOptions");
+const connectDB = require("./config/dbConn");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3500;
 
+console.log(process.env.NODE_ENV);
+
+connectDB();
+
 app.use(logger);
+
 app.use(cors(corsOptions));
+
 app.use(express.json());
+
 app.use(cookieParser());
 
-app.use("/", express.static(path.join(__dirname, "/public")));
+app.use("/", express.static(path.join(__dirname, "public")));
 
 app.use("/", require("./routes/root"));
+app.use("/users", require("./routes/userRoutes"));
+app.use("/notes", require("./routes/noteRoutes"));
 
 app.all("*", (req, res) => {
   res.status(404);
@@ -27,5 +39,18 @@ app.all("*", (req, res) => {
     res.type("txt").send("404 Not Found");
   }
 });
+
 app.use(errorHandler);
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+mongoose.connection.once("open", () => {
+  console.log("Connected to MongoDB");
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+});
+
+mongoose.connection.on("error", (err) => {
+  console.log(err);
+  logEvents(
+    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+    "mongoErrLog.log"
+  );
+});
